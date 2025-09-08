f9caa6e7c0bbe885634631f119ef5fdda1ca0dc6
Boray Gulpinar <boray112@gmail.com>
2025-09-08 13:58:24 +0300

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>{/* About component content goes here */}</div>;
+};
+
+export default About;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Resume = () => {
+  return <div>{/* Resume component content goes here */}</div>;
+};
+
+export default Resume;
